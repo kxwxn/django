164a123e9299538f78546ad2d28b3e5ea30ceafe
n0wlk <kxwxn@icloud.com>
2025-07-25 12:02:45 +0900

    control_flow added

---
+++ control_flow.py
@@ -0,0 +1,22 @@
+# COMPARISON OPERATORS
+print(10 > 3)
+print(1 > 3)
+print(10 >= 10)
+print(10 < 20)
+print(10 <= 10)
+print(10 == 10)
+print(10 != 10)
+record = "jose"
+match = "jose"
+print(record == match)
+
+# LOGICAL OPERATORS
+print(10 > 3 and 10 > 5)
+print(10 > 3 or 10 > 5)
+print(not 10 > 3)
+
+# IF STATEMENT
+
+
+
+
